5dd0d224afd2ea444ee055f6aa0d3c0f6b213077
dani <148768159+daniskate@users.noreply.github.com>
2026-02-07 13:58:27 +0100

    Update next.config.ts to add output configuration for static export to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,9 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
+module.exports = {
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      // other paths
+    }
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  output: 'export',
 };
-
-export default nextConfig;
